7a37290f2fadee8878d8bdf569ce35dd38d4c92b
krishna-sabbella <krishna.sabbella@intelliware.com>
2024-09-25 16:49:14 -0400

    using script setup

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,32 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useAuth } from './config/useAuth'
+import { myMSALObj, state } from './config/msalConfig'
+
+const { login, logout, handleRedirect } = useAuth()
+
+const handleLogin = async () => {
+  await login()
+}
+
+const handleLogout = () => {
+  logout()
+}
+
+const initialize = async () => {
+  try {
+    await myMSALObj.initialize()
+  } catch (error) {
+    console.log('Initialization error:', error)
+  }
+}
+
+onMounted(async () => {
+  await initialize()
+  await handleRedirect()
+})
+</script>
+
 <template>
   <div v-if="state.isAuthenticated">
     <div>Welcome, {{ state.user?.name }}!</div>
@@ -8,42 +37,3 @@
     <button @click="handleLogin">Log In</button>
   </div>
 </template>
-
-<script>
-import { onMounted } from 'vue'
-import { useAuth } from './config/useAuth'
-import { myMSALObj, state } from './config/msalConfig'
-
-export default {
-  setup() {
-    const { login, logout, handleRedirect } = useAuth()
-
-    const handleLogin = async () => {
-      await login()
-    }
-
-    const handleLogout = () => {
-      logout()
-    }
-
-    const initialize = async () => {
-      try {
-        await myMSALObj.initialize()
-      } catch (error) {
-        console.log('Initialization error:', error)
-      }
-    }
-
-    onMounted(async () => {
-      await initialize()
-      await handleRedirect()
-    })
-
-    return {
-      handleLogin,
-      handleLogout,
-      state
-    }
-  }
-}
-</script>
